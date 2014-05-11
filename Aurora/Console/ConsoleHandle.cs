bba3e71e1165554dccdb79b10fed0acc5f14935b
Justin Perez <booooo201@gmail.com>
2014-05-11 14:45:22 -0400

    Initial commit.

---
+++ Aurora/Console/ConsoleHandle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aurora.Console
+{
+    class ConsoleHandle
+    {
+        public void Start()
+        {
+
+        }
+
+        public void Setup()
+        {
+
+        }
+    }
+}
---
+++ Aurora/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aurora.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aurora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Loading Console handler..");
+            ConsoleHandle handle = new ConsoleHandle();
+            handle.PrintConsoleInfo();
+            handle.Start();
+        }
+    }
+}
